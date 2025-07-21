055faa1fddc561ab3722dc163123df717299542d
Priyanshugoyal018 <Priyanshugoyal533@gmail.com>
2025-07-22 00:21:07 +0530

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+bool gameOver;
+const int width = 40;   // Increased width
+const int height = 20;  // Increased height
+int x, y, fruitX, fruitY, score;
+int tailX[200], tailY[200];
+int nTail;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // For cursor control
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    COORD cursorPosition;
+    cursorPosition.X = 0;
+    cursorPosition.Y = 0;
+    SetConsoleCursorPosition(hConsole, cursorPosition); // No flicker
+
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) cout << "#";
+
+            if (i == y && j == x)
+                cout << "O"; // Head
+            else if (i == fruitY && j == fruitX)
+                cout << "F"; // Fruit
+            else {
+                bool printTail = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        printTail = true;
+                        break;
+                    }
+                }
+                if (!printTail) cout << " ";
+            }
+
+            if (j == width - 1) cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    cout << "Score: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a': dir = LEFT; break;
+            case 'd': dir = RIGHT; break;
+            case 'w': dir = UP; break;
+            case 's': dir = DOWN; break;
+            case 'x': gameOver = true; break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+        case LEFT: x--; break;
+        case RIGHT: x++; break;
+        case UP: y--; break;
+        case DOWN: y++; break;
+    }
+
+    // Wall collision wrapping
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    // Self collision
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+
+    // Eat fruit
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main() {
+    Setup();
+
+    cout << "==============================" << endl;
+    cout << "       SNAKE GAME 🐍         " << endl;
+    cout << "==============================" << endl;
+    cout << "\nControls: W A S D for Movement, X to Exit" << endl;
+    cout << "Press Enter to Start the Game...";
+    cin.get(); // Wait for Enter
+
+    // Hide blinking cursor
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+    cursorInfo.bVisible = false;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(100); // milliseconds
+    }
+
+    cout << "\nGame Over!\n";
+    return 0;
+}
